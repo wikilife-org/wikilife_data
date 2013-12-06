92eb173db38f593978f4d6c5ca5c2e8234130598
Emmanuel <ecarelli@wikilife.com>
2013-12-06 20:09:13 -0300

    profile prc

--- wikilife_data/dao/user/profile_dao.py
+++ wikilife_data/dao/user/profile_dao.py
@@ -8,15 +8,14 @@ class ProfileDAO(BaseDAO):
     Model:
     {
         "userId": "CKJG3L",
-        "updateUTC": ISODate(),
         "items" : {
-            "gender": {"nodeId": 0, "metricId": 0, value: ""},
-            "birthdate": {"nodeId": 0, "metricId": 0, value: ""},
-            "height": {"nodeId": 0, "metricId": 0, value: ""},
-            "weight": {"nodeId": 0, "metricId": 0, value: ""},
-            "country": {"nodeId": 0, "metricId": 0, value: ""},
-            "region": {"nodeId": 0, "metricId": 0, value: ""},
-            "city": {"nodeId": 0, "metricId": 0, value: ""}
+            "gender": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")},
+            "birthdate": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")},
+            "height": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")},
+            "weight": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")},
+            "country": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")},
+            "region": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")},
+            "city": {"nodeId": 0, "metricId": 0, value: "", "updateUTC": ISODate("")}
         }
     }
     """
@@ -36,21 +35,20 @@ class ProfileDAO(BaseDAO):
     def get_profile_by_user_id(self, user_id):
         return self._collection.find_one({"userId": user_id})
 
-    def get_new_profile(self, user_id, update_utc):
+    def get_blank_profile(self, user_id):
         return {
             "userId": user_id,
-            "updateUTC": update_utc,
             "items" : self.get_profile_items()
         }
 
     def get_profile_items(self):
         return {
-            "gender": {"nodeId": 271256, "metricId": 271257, "value": None},
-            "birthdate": {"nodeId": 353, "metricId": 276045, "value": None},
-            "height": {"nodeId": 271241, "metricId": 271242, "value": None},
-            "weight": {"nodeId": 271245, "metricId": 271246, "value": None},
-            "country": {"nodeId": 354, "metricId": 276047, "value": None},
-            "region": {"nodeId": 354, "metricId": 276048, "value": None},
-            "city": {"nodeId": 354, "metricId": 276049, "value": None},
-            "tz": {"nodeId": 354, "metricId": 276046, "value": None}
+            "gender": {"nodeId": 271256, "metricId": 271257, "value": None, "updateUTC": None},
+            "birthdate": {"nodeId": 353, "metricId": 276045, "value": None, "updateUTC": None},
+            "height": {"nodeId": 271241, "metricId": 271242, "value": None, "updateUTC": None},
+            "weight": {"nodeId": 271245, "metricId": 271246, "value": None, "updateUTC": None},
+            "country": {"nodeId": 354, "metricId": 276047, "value": None, "updateUTC": None},
+            "region": {"nodeId": 354, "metricId": 276048, "value": None, "updateUTC": None},
+            "city": {"nodeId": 354, "metricId": 276049, "value": None, "updateUTC": None},
+            "tz": {"nodeId": 354, "metricId": 276046, "value": None, "updateUTC": None}
         }
